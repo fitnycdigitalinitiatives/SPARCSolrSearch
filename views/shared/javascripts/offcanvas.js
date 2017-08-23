454a8c3213a4fc6ed95f7d060a1c27e3ea3abc8e
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-08-23 11:34:42 -0400

    function error

--- views/shared/javascripts/offcanvas.js
+++ views/shared/javascripts/offcanvas.js
@@ -1,5 +1,7 @@
-$(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
+jQuery(function ($) {
+  $(document).ready(function () {
+    $('[data-toggle="offcanvas"]').click(function () {
+      $('.row-offcanvas').toggleClass('active')
+    });
   });
 });

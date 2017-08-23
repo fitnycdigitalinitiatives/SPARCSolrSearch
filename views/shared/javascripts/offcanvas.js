c4c18bbed444f79e1d9bd9abc1aae65627c5db15
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-08-23 11:48:15 -0400

    hide sidebar

--- views/shared/javascripts/offcanvas.js
+++ views/shared/javascripts/offcanvas.js
@@ -1,7 +1,8 @@
 jQuery(function ($) {
   $(document).ready(function () {
     $('[data-toggle="offcanvas"]').click(function () {
-      $('.row-offcanvas').toggleClass('active')
+      $('.row-offcanvas').toggleClass('active');
+      $('.sidebar-offcanvas').toggleClass('hidden');
     });
   });
 });

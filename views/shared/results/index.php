0e3cf06ded721bd6eabfc38513f9fae8c1e7e00a
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-08-24 15:58:36 -0400

    remove footer

--- views/shared/results/index.php
+++ views/shared/results/index.php
@@ -230,9 +230,6 @@
 					<?php endforeach; ?>
 				</div>
 			</div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
     </div>
   </div>
 </div>
